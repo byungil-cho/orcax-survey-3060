94f13f815f6228fdefcf66b4768f645e26fbbca4
OrcaX <jbl01570@hanmail.net>
2025-06-12 10:34:05 +0900

    Add files via upload

--- api/harvest-barley.js
+++ api/harvest-barley.js
@@ -1,1 +1,13 @@
-1
+fetch("https://climbing-wholly-grouper.jp.ngrok.io/api/harvest-barley", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    nickname: nickname,
+    amount: yieldAmount
+  })
+})
+.then(res => res.json())
+.then(data => console.log("✅ 서버에 보리 수확 저장 완료:", data))
+.catch(err => console.error("❌ 서버 저장 실패:", err));
