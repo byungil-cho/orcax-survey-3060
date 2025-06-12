a6920cc57c3d444e468321c4fd93853f289a9e61
OrcaX <jbl01570@hanmail.net>
2025-06-12 22:31:53 +0900

    Add files via upload

---
+++ api/water-barley.js
@@ -0,0 +1,15 @@
+app.post("/api/water-barley", async (req, res) => {
+  const { nickname } = req.body;
+  const user = await User.findOne({ nickname });
+
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  if (user.water > 0) {
+    user.water -= 1;
+    user.waterGiven = (user.waterGiven || 0) + 1;
+    await user.save();
+    res.json({ success: true });
+  } else {
+    res.status(400).json({ error: "Not enough water" });
+  }
+});
