de1411c8a2153f1aba064210962a74b46ba3be36
OrcaX <jbl01570@hanmail.net>
2025-06-13 13:26:34 +0900

    Update Farm.js

--- models/Farm.js
+++ models/Farm.js
@@ -6,8 +6,7 @@ const farmSchema = new mongoose.Schema({
   fertilizer: { type: Number, default: 10 },
   token: { type: Number, default: 5 },
   barleyCount: { type: Number, default: 0 },
-  seedBarley: { type: Number, default: 2 },
-  seedPotato: { type: Number, default: 0 } // ✅ 이 줄 추가
+  seedBarley: { type: Number, default: 2 }
 });
 
 module.exports = mongoose.models.Farm || mongoose.model('Farm', farmSchema);
