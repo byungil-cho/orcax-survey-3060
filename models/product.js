f19f007e71586e5e28cf60b440f402e1fae0228e
OrcaX <jbl01570@hanmail.net>
2025-06-14 11:25:16 +0900

    Update product.js

--- models/product.js
+++ models/product.js
@@ -8,4 +8,5 @@ const ProductSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.models.Product || mongoose.model('Product', productSchema);
+
