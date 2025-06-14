5e90a928399645ea2d1c63a65b15bfea9a59c17b
OrcaX <jbl01570@hanmail.net>
2025-06-14 11:18:49 +0900

    Add files via upload

---
+++ routes/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  nickname: String,
+  type: String,         // 제품 이름
+  category: String,     // 감자 or 보리
+  count: Number         // 수량
+});
+
+module.exports = mongoose.model('Product', productSchema);
