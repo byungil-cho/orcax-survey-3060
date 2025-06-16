edef2a38a2a1cb70f106e85470bced31aaf551b1
OrcaX <jbl01570@hanmail.net>
2025-06-16 13:14:36 +0900

    Add files via upload

--- routes/userdata.js
+++ routes/userdata.js
@@ -11,7 +11,14 @@ router.get('/userdata/:nickname', async (req, res) => {
       return res.status(404).json({ success: false, message: '유저 없음' });
     }
 
-    res.json({ success: true, user });
+    res.json({
+      success: true,
+      user: {
+        ...user._doc,
+        potatoProduct: user.potatoProduct || 0,
+        barleyProduct: user.barleyProduct || 0
+      }
+    });
   } catch (err) {
     console.error('서버 오류:', err);
     res.status(500).json({ success: false, message: '서버 오류' });
