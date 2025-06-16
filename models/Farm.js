2945e517390d559d7b63ebd933a492edc8ff79c8
OrcaX <jbl01570@hanmail.net>
2025-06-16 10:24:04 +0900

    Update Farm.js

--- models/Farm.js
+++ models/Farm.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose');
 const farmSchema = new mongoose.Schema({
   nickname: String,
   farmName: String,
-  water: Number,
-  fertilizer: Number,
-  token: Number,
-  potatoCount: Number,
-  barleyCount: Number,
-  products: Array
+  water: { type: Number, default: 10 },
+  fertilizer: { type: Number, default: 10 },
+  token: { type: Number, default: 5 },
+  potatoCount: { type: Number, default: 0 },
+  barleyCount: { type: Number, default: 0 },
+  seedPotato: { type: Number, default: 2 },
+  seedBarley: { type: Number, default: 2 },
+  productStorage: { type: Array, default: [] },   // 가공된 제품 목록
+  materialStorage: { type: Array, default: [] }   // 자재 교환된 물품
 });
 
 const Farm = mongoose.model('Farm', farmSchema);
