a7e8d673fb65e0540f21a020b5622c88290baa80
OrcaX <jbl01570@hanmail.net>
2025-06-16 12:25:56 +0900

    Add files via upload

--- models/Farm.js
+++ models/Farm.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const farmSchema = new mongoose.Schema({
-  nickname: { type: String, required: true, unique: true }, // 카카오 닉네임
-  farmName: { type: String, default: "" },                  // 농장이름 (main에서 설정)
-  seed: { type: Number, default: 2 },                       // 씨감자
-  barleySeed: { type: Number, default: 2 },                 // 씨보리 ✅
-  water: { type: Number, default: 10 },                     // 물
-  fertilizer: { type: Number, default: 10 },                // 거름
-  token: { type: Number, default: 10 },                     // ORCX 토큰
-  potatoCount: { type: Number, default: 0 },                // 수확된 감자
-  barleyCount: { type: Number, default: 0 }                 // 수확된 보리
-}, { timestamps: true }); // 생성일, 수정일 자동 포함
+  nickname: { type: String, required: true, unique: true },
+  farmName: { type: String, default: "" },
+  seed: { type: Number, default: 2 },
+  barleySeed: { type: Number, default: 2 },
+  water: { type: Number, default: 10 },
+  fertilizer: { type: Number, default: 10 },
+  token: { type: Number, default: 10 },
+  potatoCount: { type: Number, default: 0 },
+  barleyCount: { type: Number, default: 0 }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Farm', farmSchema);
