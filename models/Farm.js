cd90a787c69f9cbd2834ec98ba80582c84878444
OrcaX <jbl01570@hanmail.net>
2025-06-16 11:44:39 +0900

    Add files via upload

--- models/Farm.js
+++ models/Farm.js
@@ -1,16 +1,7 @@
-const mongoose = require('mongoose');
 
-const farmSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+module.exports = mongoose.model("Farm", new mongoose.Schema({
   nickname: String,
-  water: Number,
-  fertilizer: Number,
-  token: Number,
-  potatoCount: Number,
-  barleyCount: Number,
-  seedPotato: Number,
-  seedBarley: Number
-});
-
-const Farm = mongoose.models.Farm || mongoose.model('Farm', farmSchema);
-
-module.exports = Farm;
+  farmName: String,
+  createdAt: { type: Date, default: Date.now }
+}));
