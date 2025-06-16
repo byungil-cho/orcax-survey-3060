74b0a20c1287791a8ccf1ec0075c30abb2c4c533
OrcaX <jbl01570@hanmail.net>
2025-06-16 10:51:24 +0900

    Add files via upload

--- models/Farm.js
+++ models/Farm.js
@@ -1,18 +1,16 @@
-
 const mongoose = require('mongoose');
 
 const farmSchema = new mongoose.Schema({
   nickname: String,
-  farmName: String,
-  water: { type: Number, default: 10 },
-  fertilizer: { type: Number, default: 10 },
-  token: { type: Number, default: 5 },
-  potatoCount: { type: Number, default: 0 },
-  barleyCount: { type: Number, default: 0 },
-  seedPotato: { type: Number, default: 2 },
-  seedBarley: { type: Number, default: 2 },
-  productStorage: { type: Array, default: [] },   // 가공된 제품 목록
-  materialStorage: { type: Array, default: [] }   // 자재 교환된 물품
+  water: Number,
+  fertilizer: Number,
+  token: Number,
+  potatoCount: Number,
+  barleyCount: Number,
+  seedPotato: Number,
+  seedBarley: Number
 });
 
-module.exports = mongoose.models.Farm || mongoose.model('Farm', farmSchema);
+const Farm = mongoose.models.Farm || mongoose.model('Farm', farmSchema);
+
+module.exports = Farm;
