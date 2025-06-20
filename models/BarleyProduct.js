4e7417e79204d885a244f4678c1ab58df401a872
OrcaX <jbl01570@hanmail.net>
2025-06-20 18:03:48 +0900

    Delete models directory

--- models/BarleyProduct.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const barleyProductSchema = new mongoose.Schema({
-  nickname: String,
-  product: String,
-  quantity: Number
-});
-
-module.exports = mongoose.model('BarleyProduct', barleyProductSchema);
