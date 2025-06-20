6df51fc437cccd7fac70dad49e40e184c9676728
OrcaX <jbl01570@hanmail.net>
2025-06-21 02:00:27 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI, { ... });
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
