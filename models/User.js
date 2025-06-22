f19d29c10db9ae1ab3803bd70b944b9baad03acd
OrcaX <jbl01570@hanmail.net>
2025-06-22 13:51:38 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,36 +1,13 @@
-// models/User.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  nickname: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  water: {
-    type: Number,
-    default: 10,
-  },
-  fertilizer: {
-    type: Number,
-    default: 10,
-  },
-  token: {
-    type: Number,
-    default: 10,
-  },
-  seed_potato: {
-    type: Number,
-    default: 2,
-  },
-  seed_barley: {
-    type: Number,
-    default: 2,
-  },
-  potatoCount: {
-    type: Number,
-    default: 0,
-  },
+  userId: String,
+  nickname: String,
+  potatoCount: Number,
+  water: Number,
+  fertilizer: Number,
+  token: Number,
+  inventory: [{ name: String, count: Number }]
 });
 
-module.exports = mongoose.models.User || mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
