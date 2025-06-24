727826b97dda9166b57d896611834ca82d8cbff2
OrcaX <jbl01570@hanmail.net>
2025-06-24 21:23:50 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
+
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: String,
-  nickname: String,
-  potatoCount: Number,
-  water: Number,
-  fertilizer: Number,
-  token: Number,
-  orcx: Number,
-  farmingCount: Number,
-  harvestCount: Number,
-  seedPotato: { type: Number, default: 0 },  // ✅ 씨감자
-  seedBarley: { type: Number, default: 0 },  // ✅ 씨보리
-  inventory: [{ name: String, count: Number }],
-  exchangeLogs: Array,
-  lastRecharge: Number
-});
+  별명: String,
+  오크: Number,
+  씨감자: Number,
+  물: Number,
+  비료: Number,
+  '감자 개수': Number,
+  '수확 횟수': Number,
+  '농사 개수': Number,
+  목록: Object,
+  교환로그: Object,
+  '마지막 종전': Number,
+  __: Number,
+  다섯: String
+}, { collection: 'users' });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
