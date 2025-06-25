576db86bd01774782de70fdef88148ce4756861e
OrcaX <jbl01570@hanmail.net>
2025-06-25 15:28:46 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
+const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
   kakaoId: { type: String, required: true, unique: true },
   nickname: { type: String },
-  wallet: { type: String, unique: true, sparse: true }, // 추가
+  wallet: { type: String, unique: true, sparse: true }, // ← 중복 키 문제 해결용
   seedPotato: { type: Number, default: 2 },
   seedBarley: { type: Number, default: 2 },
   water: { type: Number, default: 10 },
   fertilizer: { type: Number, default: 10 },
   token: { type: Number, default: 10 },
 });
+
+module.exports = mongoose.model('User', userSchema);
