e2eef24177848ed57e37e728a199629b292d9040
OrcaX <jbl01570@hanmail.net>
2025-06-27 21:19:15 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,17 +1,14 @@
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  nickname: { type: String, required: true },
-  orcx: { type: Number, default: 10 },
-  water: { type: Number, default: 10 },
-  fertilizer: { type: Number, default: 10 },
-  seedPotato: { type: Number, default: 0 },
-  seedBarley: { type: Number, default: 0 },
+  nickname: { type: String, required: true, unique: true },
+  orcx: { type: Number, default: 10 },       // 토큰
+  water: { type: Number, default: 10 },      // 물
+  fertilizer: { type: Number, default: 10 }, // 거름
   potatoCount: { type: Number, default: 0 },
   barleyCount: { type: Number, default: 0 },
   harvestCount: { type: Number, default: 0 },
   inventory: { type: [String], default: [] }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
