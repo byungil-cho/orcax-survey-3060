11a5739a9a5cfd2df2dd2dfb0b5e7aedb39ecf4b
OrcaX <jbl01570@hanmail.net>
2025-06-28 00:28:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3060;
 
 // 라우터 import
 const userRouter = require('./routes/user');
