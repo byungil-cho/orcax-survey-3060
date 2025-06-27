751ba3d8aa662c85c5b654e8ffb6aeba00655bc3
OrcaX <jbl01570@hanmail.net>
2025-06-27 20:02:28 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,61 +1,17 @@
-// models/User.js
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  kakaoId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  nickname: {
-    type: String,
-    required: true,
-  },
-  token: {
-    type: Number,
-    default: 10, // ✅ ORCX 토큰
-  },
-  water: {
-    type: Number,
-    default: 10,
-  },
-  fertilizer: {
-    type: Number,
-    default: 10,
-  },
-  seedPotato: {
-    type: Number,
-    default: 0,
-  },
-  seedBarley: {
-    type: Number,
-    default: 0, // ✅ 보리 씨앗
-  },
-  potatoCount: {
-    type: Number,
-    default: 0,
-  },
-  barleyCount: {
-    type: Number,
-    default: 0, // ✅ 보리 수확량
-  },
-  harvestCount: {
-    type: Number,
-    default: 0,
-  },
-  farmingCount: {
-    type: Number,
-    default: 0,
-  },
-  growthPoint: {
-    type: Number,
-    default: 0,
-  },
-  inventory: {
-    type: Array,
-    default: [],
-  },
+  nickname: { type: String, required: true },
+  orcx: { type: Number, default: 10 },
+  water: { type: Number, default: 10 },
+  fertilizer: { type: Number, default: 10 },
+  seedPotato: { type: Number, default: 0 },
+  seedBarley: { type: Number, default: 0 },
+  potatoCount: { type: Number, default: 0 },
+  barleyCount: { type: Number, default: 0 },
+  harvestCount: { type: Number, default: 0 },
+  inventory: { type: [String], default: [] }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
