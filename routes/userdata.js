b12647e194496776f1f3e9ffdb9cf4f62a88038a
OrcaX <jbl01570@hanmail.net>
2025-06-29 01:18:50 +0900

    Add files via upload

--- routes/userdata.js
+++ routes/userdata.js
@@ -1,3 +1,4 @@
+
 // api/userdata.js
 const express = require('express');
 const router  = express.Router();
@@ -5,10 +6,10 @@ const User    = require('../models/User');
 
 router.get('/', async (req, res) => {
   const { kakaoId } = req.query;
-  if (!nickname) return res.status(400).json({ success:false });
+  if (!kakaoId) return res.status(400).json({ success:false });
 
   const user = await User.findOne({ kakaoId })
-    .select('orcx seedPotato seedBarley water fertilizer');
+    .select('orcx seedPotato seedBarley water fertilizer potato inventory');
   if (!user) return res.status(404).json({ success:false });
 
   res.json({ success:true, users:[user] });
