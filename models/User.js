660acfa14831f591fd1901c473c2bc60f88c0e11
OrcaX <jbl01570@hanmail.net>
2025-07-01 01:47:32 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -4,56 +4,53 @@ const userSchema = new mongoose.Schema({
   kakaoId: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
-  nickname: {
-    type: String,
-    required: true
-  },
+  nickname: String,
   email: {
-  type: String,
-  unique: true,
-  sparse: true // ← 이거 추가!!!
+    type: String,
+    unique: true,
+    sparse: true // null 중복 허용
   },
   orcx: {
     type: Number,
-    default: 10
+    default: 10,
   },
   water: {
     type: Number,
-    default: 10
+    default: 10,
   },
   fertilizer: {
     type: Number,
-    default: 10
+    default: 10,
   },
   seedPotato: {
     type: Number,
-    default: 0
+    default: 0,
   },
   potatoCount: {
     type: Number,
-    default: 0
+    default: 0,
   },
   seedBarley: {
     type: Number,
-    default: 0
+    default: 0,
   },
   barleyCount: {
     type: Number,
-    default: 0
+    default: 0,
   },
   harvestCount: {
     type: Number,
-    default: 0
+    default: 0,
   },
   inventory: {
     type: [String],
-    default: []
+    default: [],
   },
   lastRecharge: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   }
 });
 
