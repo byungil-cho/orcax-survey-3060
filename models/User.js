3a58b770b349b092baf6579a33231d2bfd7ad734
OrcaX <jbl01570@hanmail.net>
2025-06-30 13:21:18 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,48 +1,18 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  kakaoId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  nickname: {
-    type: String,
-    required: true,
-  },
-  orcx: {
-    type: Number,
-    default: 0,
-  },
-  water: {
-    type: Number,
-    default: 0,
-  },
-  fertilizer: {
-    type: Number,
-    default: 0,
-  },
-  seedPotato: {
-    type: Number,
-    default: 0,
-  },
-  seedBarley: {
-    type: Number,
-    default: 0,
-  },
-  potato: {
-    type: Number,
-    default: 0,
-  },
-  // login.js / userdata.js 에서 사용하던 inventory 필드
-  inventory: {
-    type: [Object],
-    default: [],
-  },
-  lastUpdated: {
-    type: Date,
-    default: null,
-  },
+const UserSchema = new mongoose.Schema({
+  kakaoId:     { type: String, required: true, unique: true },
+  nickname:    String,
+  orcx:        Number,
+  water:       Number,
+  fertilizer:  Number,
+  seedPotato:  Number,
+  potatoCount: Number,
+  seedBarley:  Number,
+  barleyCount: Number,
+  harvestCount:Number,
+  inventory:   Array,
+  lastRecharge:Date
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
