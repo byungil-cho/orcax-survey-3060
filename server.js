82c7a2fe82ab3b789342d04fc6ad1ea7366ea6d0
OrcaX <jbl01570@hanmail.net>
2025-07-01 21:00:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.send("OrcaX 서버 작동 중 🐳");
 });
 
-mongoose.connect('mongodb+srv://<YOUR_MONGO_URI>', {
+mongoose.connect('mongodb+srv://<YOUR_MONGO_URL>', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
