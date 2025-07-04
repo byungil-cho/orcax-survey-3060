b0e18f772878eaaf4ee5a0465242fb96f57a1e2a
OrcaX <jbl01570@hanmail.net>
2025-07-04 11:12:37 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,57 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  kakaoId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   nickname: String,
-  email: {
-    type: String,
-    unique: true,
-    sparse: true // null 중복 허용
-  },
-  orcx: {
-    type: Number,
-    default: 10,
-  },
-  water: {
-    type: Number,
-    default: 10,
-  },
-  fertilizer: {
-    type: Number,
-    default: 10,
-  },
-  seedPotato: {
-    type: Number,
-    default: 0,
-  },
-  potatoCount: {
-    type: Number,
-    default: 0,
-  },
-  seedBarley: {
-    type: Number,
-    default: 0,
-  },
-  barleyCount: {
-    type: Number,
-    default: 0,
-  },
-  harvestCount: {
-    type: Number,
-    default: 0,
-  },
-  inventory: {
-    type: [String],
-    default: [],
-  },
-  lastRecharge: {
-    type: Date,
-    default: Date.now,
-  }
+  kakaoId: String,
+  token: Number,
+  gamja: Number,
+  sibori: Number,
 });
 
 module.exports = mongoose.model('User', userSchema);
