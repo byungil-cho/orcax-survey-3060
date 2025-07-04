069738f8aea57d31726ed8b3aa521ccdab18f61c
OrcaX <jbl01570@hanmail.net>
2025-07-04 17:16:12 +0900

    Add files via upload

--- routes/userdata.js
+++ routes/userdata.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// 사용자 정보 조회 (GET /api/userdata?kakaoId=어쩌구)
+// ✅ 사용자 정보 조회 (카카오 ID 기준)
 router.get('/', async (req, res) => {
   const kakaoId = req.query.kakaoId;
 
