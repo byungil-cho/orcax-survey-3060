3f8de6fec74fbd097dd1495fa3d39f7c17a4e449
OrcaX <jbl01570@hanmail.net>
2025-07-04 22:26:27 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -2,11 +2,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  nickname: String,
-  kakaoId: String,
-  token: Number,
-  gamja: Number,
-  sibori: Number,
+  nickname: { type: String, required: true },
+  kakaoId: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // ✅ 고유하고 필수인 이메일
+  token: { type: Number, default: 0 },
+  gamja: { type: Number, default: 0 },
+  sibori: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('User', userSchema);
