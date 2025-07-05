4bdd896595716b1e1f1a4baffb537df75a13d13b
OrcaX <jbl01570@hanmail.net>
2025-07-05 12:26:39 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   kakaoId: { type: String, required: true, unique: true },
   nickname: { type: String, default: "Unknown" },
+  email: { type: String, default: "" },
   orcx: { type: Number, default: 0 },
   water: { type: Number, default: 0 },
   fertilizer: { type: Number, default: 0 }
-  // email 제거됨
 });
 
 const User = mongoose.model("User", userSchema);
