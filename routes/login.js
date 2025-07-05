d94faf5da02f287bf5706807a1843d77c15abc53
OrcaX <jbl01570@hanmail.net>
2025-07-05 11:22:40 +0900

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getUserByKakaoId, createUser } = require("../api/user");
+const { getUserByKakaoId, createUser } = require("../api/login");
 
 router.post("/", async (req, res) => {
-  const { kakaoId } = req.body;
-  if (!kakaoId) {
-    return res.status(400).json({ success: false, message: "kakaoId 없음" });
-  }
-
   try {
+    const { kakaoId } = req.body;
+    if (!kakaoId) return res.status(400).json({ success: false, message: "카카오 ID 없음" });
+
     let user = await getUserByKakaoId(kakaoId);
     if (!user) {
-      // 기본 자원 지급
-      const newUser = {
-        kakaoId,
-        orcx: 10,
-        water: 10,
-        fertilizer: 10,
-        nickname: "신규농부" + Math.floor(Math.random() * 10000)
-      };
-      await createUser(newUser);
-      return res.json({ success: true, user: newUser });
-    } else {
-      return res.json({ success: true, user });
+      user = await createUser(kakaoId); // 물10, 비료10, 토큰10 주는 함수
     }
+
+    res.json({ success: true, user });
   } catch (err) {
-    console.error("login 에러:", err);
-    return res.status(500).json({ success: false, message: "서버 오류" });
+    console.error("로그인 에러:", err);
+    res.status(500).json({ success: false, message: "서버 오류" });
   }
 });
 
