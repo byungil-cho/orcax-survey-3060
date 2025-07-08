3d107259017229b07132b155cf97e765891f898a
OrcaX <jbl01570@hanmail.net>
2025-07-08 16:28:35 +0900

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,25 +1,24 @@
-// routes/login.js
+// api/login.js
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
 router.post('/', async (req, res) => {
   const { kakaoId } = req.body;
-
   if (!kakaoId) {
-    return res.status(400).json({ error: 'kakaoId 필요함' });
+    return res.status(400).json({ error: 'kakaoId는 필수입니다.' });
   }
 
   try {
     const user = await User.findOne({ kakaoId });
     if (!user) {
-      return res.status(404).json({ error: '유저 없음' });
+      return res.status(404).json({ error: '사용자 없음' });
     }
 
     res.status(200).json({ message: '로그인 성공', user });
   } catch (err) {
-    console.error('/api/login 오류:', err);
-    res.status(500).json({ error: '서버 내부 오류' });
+    console.error('/api/login error:', err);
+    res.status(500).json({ error: '서버 오류' });
   }
 });
 
