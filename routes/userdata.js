83f19a72598f5f1c5803433c0d370503d556f690
OrcaX <jbl01570@hanmail.net>
2025-07-11 21:18:46 +0900

    Update userdata.js

--- routes/userdata.js
+++ routes/userdata.js
@@ -3,14 +3,14 @@ router.post('/userdata', async (req, res) => {
   console.log('💬 요청받은 kakaoId:', kakaoId);
 
   try {
-    const users = await User.findOne({ kakaoId: String(kakaoId) });
+    const user = await User.findOne({ kakaoId: String(kakaoId) });
 
     if (!user) {
       return res.status(404).json({ success: false, message: 'User not found' });
     }
 
     console.log('✅ 찾은 사용자:', user);
-    res.status(200).json({ success: true, data: users });
+    res.status(200).json({ success: true, data: user });
 
   } catch (error) {
     console.error('❌ 서버 오류:', error);
