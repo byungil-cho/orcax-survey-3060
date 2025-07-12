8d73d4e85e2fc8037d68cf7d0f0acb7cf51638fe
OrcaX <jbl01570@hanmail.net>
2025-07-12 11:56:18 +0900

    Add files via upload

--- routes/userdata.js
+++ routes/userdata.js
@@ -4,22 +4,28 @@ const User = require("../models/User");
 
 router.post("/", async (req, res) => {
   const { kakaoId } = req.body;
+
   try {
     if (!kakaoId) {
-      return res.status(400).json({ success: false, message: "카카오 ID 누락" });
+      return res.status(400).json({ success: false, message: "카카오 ID 없음" });
     }
 
     const user = await User.findOne({ kakaoId });
+
     if (!user) {
-      return res.status(404).json({ success: false, message: "사용자 없음" });
+      return res.status(404).json({ success: false, message: "유저 없음" });
     }
 
     res.json({
       success: true,
-      nickname: user.nickname,
-      orcxToken: user.orcxToken,
-      inventory: user.inventory,
+      nickname: user.nickname || "닉네임 없음",
+      orcxToken: user.orcx || 0,
+      inventory: {
+        seedPotato: user.seedPotato || 0,
+        seedBarley: user.seedBarley || 0
+      }
     });
+
   } catch (err) {
     console.error("유저 데이터 오류:", err);
     res.status(500).json({ success: false, error: "서버 오류" });
