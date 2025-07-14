4066b6046a9136ee6f7e63ae14aa0a4ad50711a6
OrcaX <jbl01570@hanmail.net>
2025-07-14 22:06:55 +0900

    Add files via upload

--- routes/userdata.js
+++ routes/userdata.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/users");
 
 router.post("/", async (req, res) => {
   const { kakaoId } = req.body;
