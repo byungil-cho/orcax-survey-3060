afcd11e680592ee5273eaa5f691ddaade51b4ca5
OrcaX <jbl01570@hanmail.net>
2025-07-20 09:36:32 +0900

    Add files via upload

--- models/SeedStock.js
+++ models/SeedStock.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const SeedStockSchema = new mongoose.Schema({
   type: { type: String, enum: ['seedPotato', 'seedBarley'], required: true },
-  count: { type: Number, default: 0 }
+  count: { type: Number, default: 0 },
+  price: { type: Number, default: 2 }   // ⭐ price 필드 추가!
 });
 module.exports = mongoose.model('SeedStock', SeedStockSchema);
