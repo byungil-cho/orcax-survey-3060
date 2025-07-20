8016ae169b76e0f5a37a64019d5530f5eec542d8
OrcaX <jbl01570@hanmail.net>
2025-07-20 20:18:40 +0900

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -9,17 +9,17 @@ const userSchema = new mongoose.Schema({
   orcx: { type: Number, default: 0 },
   water: { type: Number, default: 0 },
   fertilizer: { type: Number, default: 0 },
-
   growth: {
     potato: { type: Number, default: 0 },
     barley: { type: Number, default: 0 }
   },
-
   // 🚩 감자/보리 보관함 (수확분)
   storage: {
-    gamja: { type: Number, default: 0 },  // 수확한 감자
-    bori: { type: Number, default: 0 }    // 수확한 보리
-  }
+    gamja: { type: Number, default: 0 },
+    bori: { type: Number, default: 0 }
+  },
+  // 🚩 ***가공제품 목록 필드 추가!***
+  products: { type: Object, default: {} }
 });
 
 module.exports = mongoose.models.User || mongoose.model('User', userSchema);
