f7679e1feeb045e956baff75d655ed81bfc929ae
OrcaX <jbl01570@hanmail.net>
2025-08-15 21:21:45 +0900

    Add files via upload

--- public/js/corn.js
+++ public/js/corn.js
@@ -1,19 +1,15 @@
-// public/js/corn.js
-(function(){
+(function () {
   const BASE = '/api/corn';
-
-  async function req(url, opt){
+  async function req(url, opt) {
     const res = await fetch(url, { headers:{'Content-Type':'application/json'}, credentials:'include', ...opt });
     const data = await res.json().catch(()=>({}));
-    if(!res.ok || data.ok===false) throw new Error(data.error || `HTTP ${res.status}`);
+    if (!res.ok || data.ok === false) throw new Error(data.error || `HTTP ${res.status}`);
     return data;
   }
-
-  const upsert  = (kakaoId, nickname='') => req(`${BASE}/upsert`, { method:'POST', body:JSON.stringify({ kakaoId, nickname }) });
-  const status  = (kakaoId) => req(`${BASE}/status?`+new URLSearchParams({ kakaoId }));
-  const overview= (kakaoId) => req(`${BASE}/overview?`+new URLSearchParams({ kakaoId }));
-  const update  = (kakaoId, { inc={}, set={} }={}) => req(`${BASE}/update`, { method:'POST', body:JSON.stringify({ kakaoId, inc, set }) });
+  const upsert   = (kakaoId, nickname='') => req(`${BASE}/upsert`, { method:'POST', body:JSON.stringify({ kakaoId, nickname }) });
+  const status   = (kakaoId) => req(`${BASE}/status?`+new URLSearchParams({ kakaoId }));
+  const overview = (kakaoId) => req(`${BASE}/overview?`+new URLSearchParams({ kakaoId }));
+  const update   = (kakaoId, { inc={}, set={} }={}) => req(`${BASE}/update`, { method:'POST', body:JSON.stringify({ kakaoId, inc, set }) });
   const resetAdditives = (kakaoId) => req(`${BASE}/reset-additives`, { method:'POST', body:JSON.stringify({ kakaoId }) });
-
   window.CornAPI = { upsert, status, overview, update, resetAdditives };
 })();
