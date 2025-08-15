c1ae0aa896f7458323fb86de43db4ad08488db4c
OrcaX <jbl01570@hanmail.net>
2025-08-15 21:39:51 +0900

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  kakaoId:   { type: String, required: true, unique: true },
-  nickname:  { type: String, required: true },
-  orcx:      { type: Number, default: 0 },
-  water:     { type: Number, default: 0 },
-  fertilizer:{ type: Number, default: 0 }
+  kakaoId: { type: String, required: true, unique: true },
+  nickname: { type: String, required: true },
+  orcx: { type: Number, default: 0 },
+  water: { type: Number, default: 0 },
+  fertilizer: { type: Number, default: 0 }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+// 이미 등록된 모델이 있으면 그걸 쓰고, 없으면 새로 등록
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
